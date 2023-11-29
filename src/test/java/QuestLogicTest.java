2b6a00c0c9b5c5b43b9e633324816c919a422cb1
Sergei <mre@mail.ru>
2023-11-29 21:48:28 +0800

    working version

--- src/test/java/QuestLogicTest.java
+++ src/test/java/QuestLogicTest.java
@@ -5,10 +5,8 @@ import com.javarush.quest.ryabov.entity.Quest;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
